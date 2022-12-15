99f7d1659a67e2b408f755dc394bc77d74b1cab7
Lbnkz <lucasbaixonunes@gmail.com>
2022-12-15 09:27:05 -0300

    aprendendo fundamentos

---
+++ 2_FUNDAMENTOS/fundamentos_componentes/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+//components
+import FirstComponent from "./components/FirstComponents"
+//styles / CSS
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
